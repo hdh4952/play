a1eb1541aeed13e29c1d95a89abcca577f630969
hdh4952 <gkseogml4959@gmail.com>
2020-06-26 13:21:20 +0900

    갈아엎기

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 var btnCnt = 0;
 var soundCnt = 0;
 var score = 0;
+var isImage = true;
+var havePepper = 0;
 
 function entrance() {
     document.querySelector('#status').innerHTML = "알림 > 야생의 이우영이 나타났다!!!<br>";
@@ -9,6 +11,8 @@ function entrance() {
 function bdr() {
     document.querySelector('#status').innerHTML = "알림 >";
     document.querySelector('#Image').style.border = "5px white solid";
+    btnCnt = 0;
+    soundCnt = 0;
 }
 function btnalert() {
     document.querySelector('#btnAlert').innerHTML = `
@@ -17,11 +21,17 @@ function btnalert() {
     `
 }
 function ale() {
-    document.write(`
-    <h1>으엌!!</h1><br>
-    <h3>SCORE: ${score}</h3><br>
-    <button onclick="back()">돌아가기</button>
-    `);
+    if(havePepper === 0) {
+        document.querySelector('#status').innerHTML += `
+        <div style="color: red;">보유한 고추가 없습니다!</div>
+        `
+    }else {
+        document.write(`
+        <h1>으엌!!</h1><br>
+        <h3>SCORE: ${score}</h3><br>
+        <button onclick="back()">돌아가기</button>
+        `);
+    }
 }
 function ale2() {
     document.querySelector('#status').innerHTML += "잘못된 선택입니다.<br>"
@@ -35,12 +45,32 @@ function ale2() {
 function back() {
     location.href="index.html";
 }
-function sound() {
-    document.querySelector('#status').innerHTML += "니가! 니가!<br>"; 
-    score += 10;
+function ImageClick() {
+    document.getElementById('Inventory').style.border ="5px solid black";
+    if(isImage === true){
+        document.getElementById('Image').src = "Character3.gif";
+        isImage = false;
+    }else if(isImage === false){
+        document.getElementById('Image').src = "Character1.gif";
+        isImage = true;
+    }
+    var jbRandom = Math.floor(Math.random()*10 + 1);
+    if(jbRandom === 5){
+        document.querySelector('#status').innerHTML += `<div style="color: red">고추를 얻었다!<br></div>`;
+        havePepper++;
+        score += 100;
+        Inven();        
+    }else{
+        document.querySelector('#status').innerHTML += "니가! 니가!<br>";
+        score += 10;
+    } 
     soundCnt ++;
     if(soundCnt === 15){
         document.querySelector('#status').innerHTML = "알림 > ";
         soundCnt = 0;
     }
+}
+function Inven() {
+    document.getElementById('Inventory').innerText = `현재 보유한 고추 : ${havePepper}개`
+    document.getElementById('Inventory').style.border ="10px solid red";
 }
